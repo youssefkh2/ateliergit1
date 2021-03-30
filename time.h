81411e23329cdb9d75ee2792ee67a092d2fd90a4
panza21 <80318973+panza21@users.noreply.github.com>
2021-03-30 09:26:14 +0100

    Update time.h

--- time.h
+++ time.h
@@ -1,104 +1,24 @@
-#include "time.h"
+#ifndef temps_H
+#define temps_H
+#include <SDL/SDL_image.h>
+#include <stdbool.h>
 
-SDL_Surface* chargerImage(char* imagetemps)
-{
-    SDL_Surface* image;
+typedef struct timer Timer;
+struct timer {
+    bool isStarted;
+    bool isPaused;
+    Uint32 startTime;
+    Uint32 pausedTime;
+};
 
-    image = IMG_Load(imagetemps);
-    if (!image) {
-        printf("Failed to load \"%s\"\n", imagetemps);
-        printf("IMG_Load: %s\n", IMG_GetError());
-    }
+SDL_Surface* chargerImage(char* imagetemps);
+SDL_Surface* chargerImageBMP(char* imagetemps2);
 
-    return image;
-}
+Timer initTimer();
+void startTimer(Timer*);
+void stopTimer(Timer*);
+void pauseTimer(Timer*);
+void continueTimer(Timer*);
+Uint32 getTime(Timer t);
 
-SDL_Surface* chargerImageBMP(char* imagetemps2)
-{
-    SDL_Surface *temp, *sprite;
-
-    /* Load the sprite image */
-    sprite = SDL_LoadBMP(imagetemps2);
-    if (sprite == NULL) {
-        fprintf(stderr, "Couldn't load %s: %s", imagetemps2, SDL_GetError());
-        exit(1);
-    }
-
-    /* Set transparent pixel as the pixel at (0,0) */
-    if (sprite->format->palette) {
-        SDL_SetColorKey(sprite, (SDL_SRCCOLORKEY | SDL_RLEACCEL),
-            *(Uint8*)sprite->pixels);
-    }
-
-    /* Convert sprite to video format */
-    temp = SDL_DisplayFormat(sprite);
-    SDL_FreeSurface(sprite);
-    if (temp == NULL) {
-        fprintf(stderr, "Couldn't convert background: %s\n",
-            SDL_GetError());
-        exit(1);
-    }
-    sprite = temp;
-    return sprite;
-}
-
-Timer initTimer()
-{
-    Timer t;
-    t.startTime = 0;
-    t.pausedTime = 0;
-    t.isPaused = false;
-    t.isStarted = false;
-    return t;
-}
-
-void startTimer(Timer* t)
-{
-    t->isStarted = true;
-    t->isPaused = false;
-    t->startTime = SDL_GetTicks();
-    t->pausedTime = 0;
-}
-
-void stopTimer(Timer* t)
-{
-    t->isStarted = false;
-    t->isPaused = false;
-    t->startTime = 0;
-    t->pausedTime = 0;
-}
-
-void pauseTimer(Timer* t)
-{
-    if (t->isStarted && !t->isPaused) {
-        t->isPaused = true;
-
-        t->pausedTime = SDL_GetTicks() - t->startTime;
-        t->startTime = 0;
-    }
-}
-
-void continueTimer(Timer* t)
-{
-    if (t->isStarted && t->isPaused) {
-        t->isPaused = false;
-        t->startTime = SDL_GetTicks() - t->pausedTime;
-        t->pausedTime = 0;
-    }
-}
-
-Uint32 getTime(Timer t)
-{
-    Uint32 time = 0;
-
-    if (t.isStarted) {
-        if (t.isPaused) {
-            time = t.pausedTime;
-        } else {
-            time = SDL_GetTicks() - t.startTime;
-        }
-    }
-
-    return time;
-}
-
+#endif
