42586b671a42702f79067af5e167ad1dbe5ce6ac
helamoalla <helamoalla@esprit.tngit config --global user.email helamoalla@esprit.tngit config --global user.email helamoalla@esprit.tn>
2021-04-09 20:22:30 +0100

    perso.h

--- perso.h
+++ perso.h
@@ -1,9 +1,9 @@
 #ifndef AGENCE_H_INCLUDED
 #define AGENCE_H_INCLUDED
-
+#include <SDL/SDL_ttf.h>
 typedef struct 
 {
- SDL_Surface *imageP[3][7];//////ça depend kadeh men taswiraa 
+ SDL_Surface *imageP[3][3];
 SDL_Rect posP;
 SDL_Surface *imgVie[4];
 int numvie;
@@ -17,8 +17,8 @@ double vitesse, acceleration,vitesse_max_perso;
 
 
 void initPerso(Personne *p);
-void afficherPerso(Personne p, SDL_Surface * screen);
-void movePerso(Perso *p,Uint32 dt) ;
+void afficherPerso(Personne p, SDL_Surface * screen, TTF_Font *police);
+void movePerso(Personne *p,Uint32 dt) ;
 void animerPerso (Personne* p);
-void saut (Personne* p);
+void saut(Personne* p,int dt,int posinit);
 #endif // AGENCE_H_INCLUDED
