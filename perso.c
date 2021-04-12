afd2ec3bc245e3a1ffb9b041ffc91a21e8eff395
helamoalla <helamoalla@esprit.tngit config --global user.email helamoalla@esprit.tngit config --global user.email helamoalla@esprit.tn>
2021-04-12 17:37:51 +0100

    perso.c

--- perso.c
+++ perso.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
 #include "perso.h"
-#include <SDL/SDL_ttf.h>
+
 void initPerso(Personne *p)
 {
     int x,y;
-    p->imageP[0][0]=IMG_Load("0.png");//right
+    p->imageP[0][0] =IMG_Load("0.png");//right
     p->imageP[0][1]=IMG_Load("1.png");
     p->imageP[0][2]=IMG_Load("2.png");
     p->imageP[1][0]=IMG_Load("3.png");//left
     p->imageP[1][1]=IMG_Load("4.png");
     p->imageP[1][2]=IMG_Load("5.png");
-    p->imageP[2][0]=IMG_Load("14.png");//lfouk
-    p->imageP[2][1]=IMG_Load("15.png");
-    p->imageP[2][2]=IMG_Load("16.png");
-    p->imageP[2][3]=IMG_Load("17.png");
-    p->imageP[2][4]=IMG_Load("18.png");
-    p->imageP[2][5]=IMG_Load("19.png");
-    p->imageP[2][6]=IMG_Load("20.png");
+    p->imageP[2][0]=IMG_Load("6.png");//lfouk
+    p->imageP[2][1]=IMG_Load("7.png");
+    p->imageP[2][2]=IMG_Load("8.png");
    
     p->posP.x=0;
-    p->posP.y=500;
+    p->posP.y=100;
     p->imgVie[0]=IMG_Load("vie3.png");
     p->imgVie[1]=IMG_Load("vie2.png");
     p->imgVie[2]=IMG_Load("vie1.png");
@@ -46,12 +46,12 @@ SDL_Rect positiontexte;
 	char ch[10];
     SDL_BlitSurface(p.imageP[p.ligne][p.colonne],NULL,screen,&p.posP);
     SDL_BlitSurface(p.imgVie[p.numvie],NULL,screen,&p.posV);
-
+SDL_Flip(screen);
     sprintf(ch, "score= %d ",p.score);
     texte = TTF_RenderText_Blended(police, ch , couleur);
     positiontexte.x = 500;
     positiontexte.y =100;
-
+   
 
 
 
@@ -87,12 +87,7 @@ void movePerso(Personne *p,Uint32 dt)
     }//fin if*/
 
 
-case SDL_KEYUP:
-    if(i==0 )
-    {
-        p->vitesse+=p->acceleration*dt;
-        p->posP.x-=0.5*p->acceleration*dt*dt+p->vitesse*dt;
-    }
+
 
    
 }
