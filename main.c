fed1adb010aa4ce1a7904c6c44326b83b6b527f8
Achref99Neji <80218038+Achref99Neji@users.noreply.github.com>
2021-04-13 08:58:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,40 +5,34 @@
 #include <SDL/SDL_mixer.h>
 #include "ennemi.h"
 
-int main (int argc, char const *argv[])
-{int nbre_vie=100;
-int score=0;
+int main ()
+{
+enemie e;
 SDL_Init(SDL_INIT_VIDEO);
-SDL_Window* window =   SDL_CreateWindow("test",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 100, 100,  SDL_WINDOW_SHOWN); 
+SDL_Surface  *window;
+window=SDL_SetVideoMode(1500,1000,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
+  if(window==NULL)
+    {
+        printf("Unable to set video mode : %s",SDL_GetError());
+        return 1;
+    }
+    if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER )!=0)
+    {
+        printf("Unable to inizialize SDL: %s \n",SDL_GetError());
+        return 1;
+    }
 SDL_Event event; /* La variable contenant l'événement */
 int continuer = 1; /* Notre booléen pour la boucle */
 SDL_WM_SetCaption("Gestion des événements en SDL", NULL);
-initEnnemi(Ennemi &e);
-int C=collisionBB(personne p,Ennemi e);
+void initEnnemi(enemie *e);
 while (continuer) 
-{ if (C)
-       {nbre_vie--; 
-        score--;}
-        if ((nbre_vie == 0) ou (score <0)) 
-        Continuer=0; 
-        else
-{
-        BlitEntity 
-        if (event==SDLK_RIGHT) 
-                direction =0 ;
-        else 
-            if(event==SDLK_LEFT)
-            direction =1; 
-                
-        
-        if (direction in [0..1])
-              {afficherEnnemie (Ennemi &e,SDL_Surface &screen);
-              deplacer(Ennemi &e);;
-              animerEnnemi(Ennemi &e);}
-      }
- 
+{ 
+void afficherEnnemie (enemie e,SDL_Surface *screen);
+void afficherEnnemie (enemie e,SDL_Surface *screen);
+void animerEnnemi(enemie *e);
+
 }
-   SDL_DestroyWindow(window);
+   
    SDL_Quit();    
 	return 0;
 }
