f35b7a1547f75117e9bc14c37f32b29ce2220a71
Achref99Neji <80218038+Achref99Neji@users.noreply.github.com>
2021-04-24 23:38:26 +0100

    Update ennemi.c

--- ennemi.c
+++ ennemi.c
@@ -6,7 +6,7 @@
 void initEnnemi(enemie *e)
 {
     Uint16 largeur, hauteur;
-    e->img = IMG_Load("taswira.png");
+    e->img = IMG_Load("police.jpeg");
     if (!e->img) {
         printf("Failed to load \"%s\"\n", "taswira.png");
         printf("IMG_Load: %s\n", IMG_GetError());
@@ -14,10 +14,10 @@ void initEnnemi(enemie *e)
 
    
    largeur = e->img->w / 7;
-   hauteur = e->img->h/2;
+   hauteur = e->img->h/1;
 
-    e->pos.x = 0;
-    e->pos.y = 0;
+    e->posscreen.x = 0;
+    e->posscreen.y = 0;
     
      e->animFrame .x = 0;
      e->animFrame .y = 0;
@@ -31,17 +31,17 @@ void initEnnemi(enemie *e)
 //******************************************/
 void afficherEnnemie (enemie e,SDL_Surface *screen)
 {
-SDL_BlitSurface(e.img, NULL, screen, &e.pos);
+SDL_BlitSurface(e.img, &e.animFrame, screen, &e.posscreen);
 SDL_Flip(screen);
 }
 //************************************/
 void animerEnnemi(enemie *e)
 {
-e->posscreen.y =e->direction*e->posscreen.h;//(se positionner dans la ligne correspondant a la direction)
-if( e->posscreen.x = e->img->w - e->posscreen.w )//(si dernier frame atteint)
-    e->posscreen.x =0;//(revenir au premier frame)
+e->animFrame.y =e->direction*e->animFrame.h;//(se positionner dans la ligne correspondant a la direction)
+if( e->animFrame.x = e->img->w - e->animFrame.w )//(si dernier frame atteint)
+    e->animFrame.x =0;//(revenir au premier frame)
 else
-    e->posscreen.x = e->posscreen.x + e->posscreen.w; //(avancer au frame suivant)
+    e->animFrame.x = e->animFrame.x + e->animFrame.w; //(avancer au frame suivant)
 
 }
 //**************************************************/
@@ -50,30 +50,31 @@ void deplacer (enemie *e)
 int PosMax=10;
 int PosMin=2;
 
-if (e->pos.x> PosMax)
+if (e->posscreen.x> PosMax)
     e->direction=1;
-else if(e->pos.x<PosMin)
+else if(e->posscreen.x<PosMin)
     e->direction=0;
 
 if (e->direction==0)
-    e->pos.x++ ;
+    e->posscreen.x++ ;
 else
-    e->pos.x-- ;
+    e->posscreen.x-- ;
 }
 
 //********************
-/*int collisionBB( enemie *p,enemie *e)
+int collisionBB( enemie *p,enemie *e)
 {
-if (p->pos.x + p->pos.w< p->pos.x)||
-   (p->pos.x> e->pos.x +e->pos.w)||
-   (p->pos.y + p->pos.h< e->pos.y)||
-   (p->pos.y> e->pos.y + e->pos.h )
-    collisionBB = 0; // pas de collision
+int collision;
+if ((p->posscreen.x + p->posscreen.w< p->posscreen.x)||
+   (p->posscreen.x> e->posscreen.x +e->posscreen.w)||
+   (p->posscreen.y + p->posscreen.h< e->posscreen.y)||
+   (p->posscreen.y> e->posscreen.y + e->posscreen.h ))
+    collision = 0; // pas de collision
 else
-    collisionBB = 1;
+    collision = 1;
 
-return Collision;
-}*/
+return collision;
+}
 //*********************//
 /*void deplacerIA(enemie *e);*/
 //**********************//
